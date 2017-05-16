8c3389e9821add886d98862513eae810083a72e0
Renan <renanctr@gmail.com>
2017-05-16 13:08:12 -0400

    changes

--- src/main/java/example/controller/UserController.java
+++ src/main/java/example/controller/UserController.java
@@ -6,12 +6,7 @@
 package example.controller;
 
 import example.model.User;
-import example.service.AccountService;
-import example.service.SecretService;
 import example.service.UserService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import example.validator.UserValidator;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  *
@@ -34,32 +31,23 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
-    
-    @Autowired
-    SecretService secretService;
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    AccountService accountService;
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
         return "welcome";
     }
 
-    @RequestMapping(path = "/createaccount", method = RequestMethod.POST)
-    public @ResponseBody
-    String createAccount(@RequestParam("username") String userName, @RequestParam("password") String password, @RequestParam("passwordConfirm") String passwordConfirm, BindingResult bindingResult) {
+    @RequestMapping(path = "/createaccount", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public String createAccount(@RequestParam("username") String userName, @RequestParam("password") String password, @RequestParam("confirmPassword") String passwordConfirm) {
 
         User user = null;
 
         try {
-
-            userValidator.validate(user, bindingResult);
+            
             user = new User(userName, password, passwordConfirm);
-
             userService.save(user);
 
         } catch (Exception ex) {
@@ -71,21 +59,6 @@ public class UserController {
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public @ResponseBody
     String login(@RequestParam("username") String userName, @RequestParam("password") String password) {
-
-        Date now = new Date();
-        Date exp = new Date(System.currentTimeMillis() + (1000 * 420));
-
-        String jwt = Jwts.builder()
-                .setHeaderParam("kid", secretService.getMyPublicCreds().getKid())
-                .setIssuedAt(now)
-                .setIssuer(userName)
-                .setNotBefore(now)
-                .setExpiration(exp)
-                .signWith(
-                        SignatureAlgorithm.RS256,
-                        secretService.getMyPrivateKey()
-                )
-                .compact();
 
         return "redirect:welcome";
 
--- src/main/java/example/model/User.java
+++ src/main/java/example/model/User.java
@@ -5,14 +5,10 @@
  */
 package example.model;
 
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
